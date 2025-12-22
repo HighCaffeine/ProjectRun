8e59ee22689a093ac94c7dfdbf4db750b7059a07
HighCaffeine <ghwns9099@naver.com>
2025-12-22 13:00:08 +0900

    [Feat] Wwise Event, Ambient Test Code

---
+++ Client/Assets/_Project/Scripts/Sound/WwiseTest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlaySoundScript : MonoBehaviour
+{
+    public AK.Wwise.Event myCustomEvent;
+    [SerializeField] private AkAmbient ambientComponent;
+
+    public bool useAmbient;
+    void Update()
+    {
+        // 예: 스페이스바를 누를 때 이벤트 호출
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 이벤트를 이 게임 오브젝트에서 실행
+            myCustomEvent.Post(gameObject);
+
+            if (useAmbient) PlayNow();
+        }
+    }
+
+    public void PlayNow()
+    {
+        ambientComponent.HandleEvent(gameObject);
+    }
+}
