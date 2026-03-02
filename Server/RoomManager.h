8e0350d9673ba2faad0ea9d397b36b6e43199768
HighCaffeine <ghwns9099@naver.com>
2026-03-02 23:22:50 +0900

    [Fix] Server Room Physics update fix

--- Server/RoomManager.h
+++ Server/RoomManager.h
@@ -16,13 +16,15 @@ public:
 		mRoomList = std::vector<Room*>(maxRoomCount_);
 
 		// temp
-		const std::string navMeshFileName("all_tiles_navmesh.bin");
+		const std::string navMeshFileName("all_tiles_tilecache.bin");
+
+		NavMeshManager::GetInstance()->Init(navMeshFileName);
 
 		for (auto i = 0; i < maxRoomCount_; i++)
 		{
 			mRoomList[i] = new Room();
 			mRoomList[i]->SendPacketFunc = SendPacketFunc;
-			mRoomList[i]->Init((i+ beginRoomNumber_), maxRoomUserCount_, navMeshFileName);
+			mRoomList[i]->Init((i + beginRoomNumber_), maxRoomUserCount_, navMeshFileName);
 		}
 	}
 
