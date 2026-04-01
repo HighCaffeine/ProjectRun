7d8835a95f7181ad97a59d4277dc129166f88436
desktop <ghwns9099@naver.com>
2026-04-01 23:51:57 +0900

    [Fix] Client login packet fix

--- Assets/_Project/Scripts/ServerTest/Backend/Client.cs
+++ Assets/_Project/Scripts/ServerTest/Backend/Client.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public static unsafe class Client
 {
     //private const string IP = "127.0.0.1";
+    /// <summary>
     private const string IP = "20.196.66.227";
+    /// </summary>
     //public static NetworkClient TCP = new NetworkClient(IP, 5004, ProtocolType.Tcp);
     public static NetworkClient TCP = new NetworkClient(IP, 11021, ProtocolType.Tcp);
     public static NetworkClient UDP = new NetworkClient(IP, 5025, ProtocolType.Udp);
