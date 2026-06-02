285223378493ec982c73f374281a8a7305c07110
desktop <ghwns9099@naver.com>
2026-06-02 14:31:41 +0900

    [Fix] Lobby UI Color, User Info

--- Assets/_Project/Scripts/Lobby/RoomListItem.cs
+++ Assets/_Project/Scripts/Lobby/RoomListItem.cs
@@ -12,8 +12,13 @@ public class RoomListItem : MonoBehaviour
     [Tooltip("게임상태")][SerializeField] private TextMeshProUGUI statusText;
     [Tooltip("참가버튼")][SerializeField] private Button joinButton;
 
-    [SerializeField] private Image hostSlotBox;
-    [SerializeField] private Image guestSlotBox;
+    [SerializeField] private Image hostPlayerBlockImage;
+    [SerializeField] private Image hostPlayerImage;
+    [SerializeField] private Image guestPlayerBlockImage;
+    [SerializeField] private Image guestPlayerImage;
+
+    private Image hostSlotBox;
+    private Image guestSlotBox;
 
     private int myRoomNum;
     private int beforePingIndex = -1;
@@ -30,71 +35,100 @@ public class RoomListItem : MonoBehaviour
 
         if (statusText != null)
         {
-            statusText.text = isPlaying ? "<color=red>게임 중</color>" : "<color=#00FF00>대기 중</color>";
+            statusText.text = isPlaying ? "<color=#A91929>게임 중</color>" : "<color=#332B28>대기 중</color>";
         }
 
         if (pingText != null)
         {
             int currentPing = info.hostPing; // 서버와 통신해서 계산된 실제 핑
 
-            string pingColor = string.Empty;
+            //string pingColor = string.Empty;
             int pingIndex = 0;
             if (currentPing <= 50)
             {
-                pingColor = "#00FF00";
+                //pingColor = "#00FF00";
                 pingIndex = 0;
             }
             else if (currentPing <= 100)
             {
-                pingColor = "#FFFF00";
+                //pingColor = "#FFFF00";
                 pingIndex = 1;
             }
             else
             {
-                pingColor = "#FF0000";
+                //pingColor = "#FF0000";
                 pingIndex = 2;
             }
 
             if (beforePingIndex != pingIndex)
             {
-                ColorUtility.TryParseHtmlString(pingColor, out Color imageColor);
+                //ColorUtility.TryParseHtmlString(pingColor, out Color imageColor);
                 foreach (var obj in pingImages) obj.gameObject.SetActive(false);
 
                 pingImages[pingIndex].gameObject.SetActive(true);
-                pingImages[pingIndex].color = imageColor;
+                //pingImages[pingIndex].color = imageColor;
                 beforePingIndex = pingIndex;
             }
 
-            pingText.text = $"<color={pingColor}>{currentPing}ms</color>";
+            //pingText.text = $"<color={pingColor}>{currentPing}ms</color>";
         }
 
-        if (hostSlotBox != null)
+        if (hostPlayerBlockImage != null)
         {
-            hostSlotBox.color = Color.green;
+            hostPlayerImage.gameObject.SetActive(true);
+            hostPlayerBlockImage.gameObject.SetActive(false);
         }
 
-        // 파티원 슬롯 처리
-        if (guestSlotBox != null)
+        if (guestPlayerBlockImage != null)
         {
             if (info.curUser == 1)
             {
-                // 빈 슬롯 상태
-                guestSlotBox.color = new Color(0.5f, 0.5f, 0.5f, 0.25f);
+                guestPlayerBlockImage.gameObject.SetActive(true);
+                guestPlayerImage.gameObject.SetActive(false);
             }
             else if (info.curUser == 2)
             {
                 if (info.guestReadyState == 2)
                 {
-                    // 파티원이 준비를 완료한 상태 -> 초록박스
-                    guestSlotBox.color = Color.green;
+                    guestPlayerBlockImage.gameObject.SetActive(false);
                 }
                 else
                 {
-                    // 파티원이 들어왔으나 준비 안 한 상태 -> 빨간박스 (기본값)
-                    guestSlotBox.color = Color.red;
+                    guestPlayerImage.gameObject.SetActive(true);
+                    guestPlayerBlockImage.gameObject.SetActive(true);
                 }
             }
         }
+
+        // if (hostSlotBox != null)
+        // {
+        //     hostSlotBox.color = Color.green;
+        // }
+
+        // // 파티원 슬롯 처리
+        // if (guestSlotBox != null)
+        // {
+        //     if (info.curUser == 1)
+        //     {
+        //         // 빈 슬롯 상태
+        //         guestSlotBox.color = new Color(0.5f, 0.5f, 0.5f, 0.25f);
+        //     }
+        //     else if (info.curUser == 2)
+        //     {
+        //         if (info.guestReadyState == 2)
+        //         {
+        //             // 파티원이 준비를 완료한 상태 -> 초록박스
+        //             ColorUtility.TryParseHtmlString("#332B28", out Color imageColor);
+        //             guestSlotBox.color = imageColor;
+        //         }
+        //         else
+        //         {
+        //             // 파티원이 들어왔으나 준비 안 한 상태 -> 빨간박스 (기본값)
+        //             ColorUtility.TryParseHtmlString("#A91929", out Color imageColor);
+        //             guestSlotBox.color = imageColor;
+        //         }
+        //     }
+        // }
 
         if (joinButton != null)
         {
